71ed06c79e0676aa05d18874efb02ea4b762bfd1
Sebastian Gilits <sep.gil@gmail.com>
2017-12-18 19:53:23 +0100

    Document the API function.

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,18 @@ import 'whatwg-fetch';
 
 const API_URL = 'https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=15';
 
+/**
+ * Fetches the crypto currencies from coinmarketcap.com and returns the most
+ * important data as an array in an promise.
+ */
 export function fetchCurrencies() {
+  // Fetch the data.
   return fetch(API_URL)
     .then(function(response) {
+      // Get the response as json.
       return response.json()
     }).then(function (data) {
+      // return only the data we really need.
       return data.map((coin) => {
         return {
           name: coin['name'],
